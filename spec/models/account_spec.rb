5290286c9fc485be71e2b1c4d5e9d0b9bfd943c4
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-19 15:53:36 +0100

    spec'ed validators on account model for email and subdomain

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,21 @@ require 'rails_helper'
 describe Account do
   let(:account) { create :account }
 
+  let(:invalid_account) do
+    ->(params) { Account.create params }
+  end
+
   describe '#create' do
+    it 'is invalid without a subdomain' do
+      expect(invalid_account[ email: Faker::Internet.email ]).to_not be_valid
+    end
+
+    it 'is invalid without an email address' do
+      expect(invalid_account[ subdomain: Faker::StarWars.planet ]).to_not be_valid
+    end
+
+    it 'is valid with email and subdomain' do
+      expect(account).to be_valid
+    end
   end
 end

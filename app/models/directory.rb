f7f822a3a5f9e037b1933fce1209504beb20fb73
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-21 21:53:50 +0100

    Refactor recursion method

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,8 +1,9 @@
 class Directory < ApplicationRecord
   belongs_to :user
-  belongs_to :parent, class_name: 'Directory', foreign_key: :directory_id, inverse_of: :children, required: false
 
+  belongs_to :parent, class_name: 'Directory', foreign_key: :directory_id, inverse_of: :children, required: false
   has_many :children, class_name: 'Directory', foreign_key: :directory_id, inverse_of: :parent
+
   has_many :records
 
   before_validation :search_for_owner
@@ -16,35 +17,48 @@ class Directory < ApplicationRecord
   end
 
   def recurse
-    local_attrs = attributes
-    local_attrs[:records] = records.inject([]) do |array, record|
-      array << record.attributes
-    end
-    local_attrs[:children] = []
-    unless children.empty?
-      children.each do |child|
-        local_attrs[:children] << child.recurse
+    tree_node.tap do |node|
+      unless children.empty?
+        children.each do |child|
+          node[:children] << child.recurse
+        end
       end
     end
-    local_attrs
   end
 
   private
+
+  def tree_node
+    attributes.merge(records: records_attributes, children: [])
+  end
+
+  def records_attributes
+    records.inject([]) do |array, record|
+      array << record.attributes
+    end
+  end
+
+  # ##############
+  # # Validation #
+  # ##############
 
   def singularity_of_root
     self.errors.add(:singularity_of_root, 'This user already has a root directory') if user.root.present? && directory_id.nil?
   end
 
+  # #########
+  # # Hooks #
+  # #########
+
   def search_for_owner
     unless directory_id.nil?
-      until user.present?
-        set_owner(parent)
+      current_directory = self
+      until current_directory.directory_id.nil?
+        current_directory = current_directory.parent
+        break if current_directory.user.present?
       end
+      self.user = current_directory.user
     end
-  end
-
-  def set_owner(parent_directory)
-    self.user = parent_directory.user
   end
 
 end

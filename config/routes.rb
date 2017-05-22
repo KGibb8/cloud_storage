f8ef36d25a14224520c2946fbdd4018ab7b0861a
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-22 15:59:50 +0100

    Subdomain matchers to set routes to account subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present?
+  end
+end
+
+class SubdomainAbsent
+  def self.matches?(request)
+    request.subdomain.blank?
+  end
+end
+
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'accounts#index'
-  resources :accounts, only: [:index, :create, :show]
+
+  constraints SubdomainPresent do
+    root to: 'accounts#show'
+  end
+
+  constraints SubdomainAbsent do
+    root to: 'accounts#index'
+    resources :accounts, only: [:index, :create]
+  end
 
 end

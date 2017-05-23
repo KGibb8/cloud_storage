392cb59cca66e2ffd917747698877e6a6b0b5fbf
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-23 15:49:05 +0100

    Validate account scoped to user on account_user

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -3,5 +3,5 @@ class AccountUser < ApplicationRecord
   belongs_to :account
 
   validates_presence_of :user, :account
-  validates_uniqueness_of :user, :account
+  validates :account, uniqueness: { scope: [:user_id], message: "User already has access to account" }
 end

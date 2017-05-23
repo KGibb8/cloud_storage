bdf0ef188eba9171e74d3aa1530a993f92d63783
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-23 15:05:15 +0100

    Set current_account or reset to nil using subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
+require "#{Rails.root}/app/lib/subdomain_matcher"
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  attr_reader :current_user
 
   before_action :current_account, if: Proc.new { SubdomainPresent.matches?(request) }
-  # before_action :authenticate_user!
+  before_action :reset_current_account, if: Proc.new { SubdomainAbsent.matches?(request) }
 
   def current_account
     @current_account ||= Account.find_by(subdomain: request.subdomain)
   end
+
+  def reset_current_account
+    @current_account = nil
+  end
+
+  def authenticate_user_account!
+    @current_account_user ||= AccountUser.find_by(account: current_account, user: current_user)
+  end
+
 end

fa259a18284f5639dbe6671dbf3dc69c70657334
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-26 12:34:32 +0100

    Configure to use port 5000 for development purposes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use DomainGenerator::CustomDomainCookie, ".#{ENV.fetch( 'APP_DOMAIN' )}"
+  hconfig.middleware.use DomainGenerator::CustomDomainCookie, ".#{ENV.fetch( 'APP_DOMAIN' )}"
 end

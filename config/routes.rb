1b8bba8b60a90125d73bcc47e49eb1aadad42732
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-27 09:45:47 +0100

    Change root of subdomain to directories index

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   constraints DomainGenerator::SubdomainPresent do
-    root to: 'accounts#show'
-    resources :accounts, only: [:update, :destroy]
-    resources :directories, only: [:index, :create, :update, :destroy]
+    root to: 'directories#index'
+    resources :accounts, only: [:show, :update, :destroy]
+    resources :directories, only: [:create, :update, :destroy]
     resources :records, only: [:create, :update, :destroy]
   end
 
   constraints DomainGenerator::SubdomainAbsent do
     root to: 'accounts#index'
     resources :accounts, only: [:index, :create]
+    resources :directories, only: [:index]
   end
 
 end

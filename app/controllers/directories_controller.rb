b174f6601bdb893fee0d5c0fccfeabbe73393927
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-28 12:30:06 +0100

    Ajax request to get#show directory with tree

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -1,11 +1,16 @@
 class DirectoriesController < ApplicationController
   before_action :find_root_directory, only: [:index]
+  before_action :find_directory, only: [:show]
 
   def index
   end
 
   def create
     directory = Directory.create(directory_params)
+  end
+
+  def show
+    render json: @directory.tree.to_json
   end
 
   private
@@ -16,6 +21,10 @@ class DirectoriesController < ApplicationController
 
   def find_root_directory
     @root = Directory.find_by(directory_id: nil)
+  end
+
+  def find_directory
+    @directory = Directory.find(params[:id])
   end
 
 end

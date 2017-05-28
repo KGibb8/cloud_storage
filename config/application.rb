1cdc92e042b28172ee2491fcf3bfedd043aec910
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-28 14:04:06 +0100

    Ensure domain middleware loaded pre-rails init

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,15 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require 'apartment/elevators/subdomain'
 
+Dir['./app/lib/domain_generator/*.rb'].each { |file| require file }
+
 Bundler.require(*Rails.groups)
 
 module CloudStorage
   class Application < Rails::Application
+    config.autoload_paths += %W(#{Rails.root}/app/middleware #{Rails.root}/app/lib)
+
     config.middleware.use Apartment::Elevators::Subdomain
     config.middleware.use DomainGenerator::CustomDomainCookie, ".#{ENV.fetch( 'APP_DOMAIN' )}"
-
-    config.autoload_paths += %W(#{Rails.root}/app/middleware #{Rails.root}/app/lib)
   end
 end

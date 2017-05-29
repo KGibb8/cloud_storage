06418370f63f7be85d71d4860264d36feca4ccf3
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-05-29 22:06:33 +0100

    spec after_destroy hook, full coverage for account

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -27,19 +27,29 @@ describe Account do
   describe '#create_with_new_user' do
     let(:account) { Account.create_with_new_user(account_params, user_params) }
 
-    it 'returns a new account' do
-      expect(account).to_not be_nil
-      expect(account).to be_a_kind_of Account
+    context 'with valid creation criteria' do
+      it 'returns a new account' do
+        expect(account).to_not be_nil
+        expect(account).to be_a_kind_of Account
+      end
+
+      it 'creates a new user' do
+        expect(account.users.first).to be_valid
+        expect(account.users.count).to be 1
+      end
+
+      it 'creates a account_user' do
+        expect(account.account_users.first).to be_valid
+        expect(account.account_users.count).to be 1
+      end
     end
 
-    it 'creates a new user' do
-      expect(account.users.first).to be_valid
-      expect(account.users.count).to be 1
-    end
+    context 'with invalid creation criteria' do
+      let(:invalid) { Account.create_with_new_user({subdomain: 'woof'}, {email: 'smeg'}) }
 
-    it 'creates a account_user' do
-      expect(account.account_users.first).to be_valid
-      expect(account.account_users.count).to be 1
+      it 'returns nil if rollback' do
+        expect(invalid).to be_nil
+      end
     end
   end
 
@@ -47,15 +57,32 @@ describe Account do
     let(:user) { create :user }
     let(:account) { Account.create_with_current_user(account_params, user.id) }
 
-    it 'returns a new account' do
-      expect(account).to_not be_nil
-      expect(account).to be_a_kind_of Account
+    context 'with valid creation criteria' do
+      it 'returns a new account' do
+        expect(account).to_not be_nil
+        expect(account).to be_a_kind_of Account
+      end
+
+      it 'creates a account_user' do
+        expect(account.account_users.first).to be_valid
+        expect(account.account_users.count).to be 1
+      end
     end
 
-    it 'creates a account_user' do
-      expect(account.account_users.first).to be_valid
-      expect(account.account_users.count).to be 1
+    context 'with invalid creation criteria' do
+      let(:invalid) { Account.create_with_current_user({subdomain: 'woof'}, user.id) }
+
+      it 'returns nil if rollback' do
+        expect(invalid).to be_nil
+      end
     end
   end
 
+  describe 'deleting an account' do
+    let(:delete) { account.destroy }
+
+    it 'drops the equivalent schema' do
+      expect(Account.pluck(:subdomain)).to_not include delete.subdomain
+    end
+  end
 end

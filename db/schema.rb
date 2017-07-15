d9b59a37e636c5e177b6b6f6b45b5aa73e5138dc
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-07-15 21:58:24 +0100

    Add name column to directory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522090617) do
+ActiveRecord::Schema.define(version: 20170715195406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170522090617) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "directory_id"
+    t.string   "name"
     t.index ["directory_id"], name: "index_directories_on_directory_id", using: :btree
     t.index ["user_id"], name: "index_directories_on_user_id", using: :btree
   end
